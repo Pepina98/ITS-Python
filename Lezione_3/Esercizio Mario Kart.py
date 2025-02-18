42dda78fa0a177abc3ce28d8942f840cd253dfc2
Pepina98 <rachelecorsi10@gmail.com>
2025-02-18 13:13:00 +0100

    Esercizi Mario Kart

---
+++ Lezione_3/Esercizio Mario Kart.py
@@ -0,0 +1,18 @@
+#Esercizio Mario Kart
+
+n:int = int(input(" Inserisci il numero: "))
+
+n>0
+
+if n==1:
+    print(f"{n}st")
+
+elif n==2:
+    print(f"{n}nd")
+
+elif n==3:
+    print(f"{n}rd")
+
+else:
+    print(f"{n}th")
+
