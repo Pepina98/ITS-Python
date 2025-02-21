224ddd0f45e224175340d66dc915e236f7444b8f
Pepina98 <rachelecorsi10@gmail.com>
2025-02-21 10:36:46 +0100

    Esercizi

---
+++ Lezione_3.1/#Esercizio 5-003.py
@@ -0,0 +1,12 @@
+#Esercizio 5-3
+alien_color = str(input("Inserisci un colore:"))
+alien_color == "green"
+
+if alien_color == "green":
+    print("Il giocatore vince 5 punti")
+
+else:
+    print("Il giocatore ha perso")
+
+if alien_color == "red":
+    print("")
