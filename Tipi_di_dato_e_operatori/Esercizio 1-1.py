87116b03f21728e536fff135c3d1570a61811555
Pepina98 <rachelecorsi10@gmail.com>
2025-02-25 18:32:33 +0100

    Esercizio

---
+++ Tipi_di_dato_e_operatori/Esercizio 1-1.py
@@ -0,0 +1,10 @@
+#Esercizio 1-1 
+x:float = 3.5
+
+y:float = 1.0/x
+
+prodotto:float = x * y 
+
+sottrazione:float = prodotto - x 
+
+print(f"Il valore di x è: {x}\nIl valore di y è: {y}\nIl valore del prodotto è:{prodotto}\nIl valore della sottrazione è:{sottrazione}")
