1d700997618b959ac1ee19d2c58737c14ccde3b2
Pepina98 <rachelecorsi10@gmail.com>
2025-03-03 19:16:32 +0100

    esercizi

---
+++ Tipi_di_dato_operatori_collection/Esercizio 3-5.py
@@ -0,0 +1,10 @@
+#Esercizio 3-5
+names:list[str] = ["Rebecca", "Aurora", "Camilla"]
+
+names[1]= "Ilaria"
+
+
+
+
+print(f"{names[0]},would you want come to dinner?\n{names[1]},would you want come to dinner?\n{names[2]},would you want come to dinner?")
+print("Aurora can't come to dinner")
