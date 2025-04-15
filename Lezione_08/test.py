f80a67b27424282b383cd96c7cb977456a0c9c35
Pepina98 <rachelecorsi10@gmail.com>
2025-04-15 13:05:10 +0200

    es

--- Lezione_08/test.py
+++ Lezione_08/test.py
@@ -2,4 +2,10 @@ from persona import Persona
 from studente import Studente
 
 # creo un oggetto p della classe Persona
-p: Persona 
+p: Persona = Persona("Rachele", "Corsi", 27)
+
+# visualizzare le informazioni dell'oggetto p
+print(p)
+
+# creare un oggetto studente1 della classe Studente
+studente1: Studente = Studente("Mario", "Rossi", 20 ," ")
